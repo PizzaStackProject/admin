4c7e1f4a7b8479e5d7abddab1895d7baee7e0465
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2023-09-08 01:48:33 +0200

    add auth to admin

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,37 @@ import buildHasuraProvider from "ra-data-hasura";
 import { MenuList } from "@app/modules/menu/components/menu-list/menu-list.component";
 import { MenuEdit } from "@app/modules/menu/components/menu-edit/menu-edit.component";
 import { MenuCreate } from "@app/modules/menu/components/menu-create/menu-create.component";
+import { authProvider } from "./core/auth-provider";
+import { apolloClient } from "./core/apollo-client";
 
 export const App = () => {
-  const [dataProvider, setDataProvider] = useState<DataProvider<string> | null>(null);
+  const [dataProvider, setDataProvider] = useState<DataProvider<string> | null>(
+    null
+  );
 
   useEffect(() => {
     const buildDataProvider = async () => {
       const dp = await buildHasuraProvider({
-        clientOptions: { uri: "http://localhost:8080/v1/graphql" },
+        client: apolloClient,
       });
       setDataProvider(dp);
     };
 
-
     buildDataProvider();
   }, []);
 
-  if (!dataProvider) 
-    return <Loading />;
+  if (!dataProvider) return <Loading />;
 
   return (
     <>
       <CssBaseline />
-      <Admin dataProvider={dataProvider}>
-        <Resource name="menu" list={MenuList} edit={MenuEdit} create={MenuCreate}/>
+      <Admin dataProvider={dataProvider} authProvider={authProvider} requireAuth>
+        <Resource
+          name="menu"
+          list={MenuList}
+          edit={MenuEdit}
+          create={MenuCreate}
+        />
       </Admin>
     </>
   );
