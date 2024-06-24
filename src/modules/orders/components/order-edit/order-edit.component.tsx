3428fb6cb50881cc559ce83b544ec6cdc107ca3a
Yaroslav Ustinov <69744663+Yaroslaw07@users.noreply.github.com>
2024-06-24 18:59:09 +0200

    Refactor data-provider and order modules

---
+++ src/modules/orders/components/order-edit/order-edit.component.tsx
@@ -0,0 +1,19 @@
+import { ReferenceInput, SelectInput } from "react-admin";
+
+import { Edit, SimpleForm, TextInput } from "react-admin";
+
+export const OrderEdit = () => {
+  return (
+    <Edit mutationMode="pessimistic">
+      <SimpleForm>
+        <TextInput source="client_address" label="Client Address" />
+        <TextInput source="client_name" label="Client Name" />
+        <TextInput source="client_phone" label="Client Phone" />
+        <TextInput source="id" />
+        <ReferenceInput reference="order_status" source="status" label="Status">
+          <SelectInput optionText="label" />
+        </ReferenceInput>
+      </SimpleForm>
+    </Edit>
+  );
+};
