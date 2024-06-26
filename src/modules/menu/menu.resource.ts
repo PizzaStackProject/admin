1f8255a3a365418061389de956ece92ffafec93a
Yaroslav Ustinov <69744663+Yaroslaw07@users.noreply.github.com>
2024-06-26 14:36:08 +0200

    Fix issue with incorrect displaying menu list

--- src/modules/menu/menu.resource.ts
+++ src/modules/menu/menu.resource.ts
@@ -1,9 +1,9 @@
-import { MenuList } from "@mui/material";
 import { MenuEdit } from "./components/menu-edit/menu-edit.component";
 import { MenuCreate } from "./components/menu-create/menu-create.component";
 
 import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
 import { ResourceProps } from "react-admin";
+import { MenuList } from "./components/menu-list/menu-list.component";
 
 export const MenuResource: ResourceProps = {
   name: "menu",
